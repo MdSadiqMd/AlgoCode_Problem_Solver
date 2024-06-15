2734527c29e22e8fa554b7c0e1a7e45467f7c6d8
Md.Sadiq <mohammadsadiq4950@gmail.com>
2024-06-15 18:41:17 +0530

    feat: added error codes

--- src/index.ts
+++ src/index.ts
@@ -8,10 +8,10 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 
-app.get("/api", apiRouter);
+app.use("/api", apiRouter);
 
 app.get("/ping", (req: Request, res: Response) => {
-  return res.json({ message: "Pong from problem service" });
+  return res.json({ message: "Pong from root" });
 });
 
 app.listen(PORT, () => {
