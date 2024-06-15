c50a3f0f252991c14f53f3a56205efa2914d27ee
Md.Sadiq <mohammadsadiq4950@gmail.com>
2024-06-15 19:55:09 +0530

    feat: added error classes

--- src/controllers/problem.controller.ts
+++ src/controllers/problem.controller.ts
@@ -1,39 +1,18 @@
 import { Request, Response } from "express";
-import { StatusCodes } from "http-status-codes";
 
 function pingProblemController(req: Request, res: Response) {
   return res.json({ message: "pong problem controller" });
 }
 
-function addProblem(req: Request, res: Response) {
-  return res
-    .status(StatusCodes.NOT_IMPLEMENTED)
-    .json({ message: "not implemented" });
-}
+function addProblem(req: Request, res: Response) {}
 
-function getProblem(req: Request, res: Response) {
-  return res
-    .status(StatusCodes.NOT_IMPLEMENTED)
-    .json({ message: "not implemented" });
-}
+function getProblem(req: Request, res: Response) {}
 
-function getProblems(req: Request, res: Response) {
-  return res
-    .status(StatusCodes.NOT_IMPLEMENTED)
-    .json({ message: "not implemented" });
-}
+function getProblems(req: Request, res: Response) {}
 
-function deleteProblem(req: Request, res: Response) {
-  return res
-    .status(StatusCodes.NOT_IMPLEMENTED)
-    .json({ message: "not implemented" });
-}
+function deleteProblem(req: Request, res: Response) {}
 
-function updateProblem(req: Request, res: Response) {
-  return res
-    .status(StatusCodes.NOT_IMPLEMENTED)
-    .json({ message: "not implemented" });
-}
+function updateProblem(req: Request, res: Response) {}
 
 module.exports = {
   pingProblemController,
