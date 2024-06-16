339d18e3a4eaf8e88afe76c4288abfb287976728
Md.Sadiq <mohammadsadiq4950@gmail.com>
2024-06-16 15:10:57 +0530

    feature: error handleing in problem controller

--- src/controllers/problem.controller.ts
+++ src/controllers/problem.controller.ts
@@ -1,18 +1,49 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
+const NotImplemented = require("../errors/notimplemented.error");
 
 function pingProblemController(req: Request, res: Response) {
   return res.json({ message: "pong problem controller" });
 }
 
-function addProblem(req: Request, res: Response) {}
+function addProblem(req: Request, res: Response, next: NextFunction) {
+  try {
+    throw new NotImplemented("add Problem");
+  } catch (error) {
+    next(error);
+  }
+}
 
-function getProblem(req: Request, res: Response) {}
+function getProblem(req: Request, res: Response, next: NextFunction) {
+  try {
+    throw new NotImplemented("get Problem");
+  } catch (error) {
+    next(error);
+  }
+}
 
-function getProblems(req: Request, res: Response) {}
+function getProblems(req: Request, res: Response, next: NextFunction) {
+  try {
+    throw new NotImplemented("get Problems");
+  } catch (error) {
+    next(error);
+  }
+}
 
-function deleteProblem(req: Request, res: Response) {}
+function deleteProblem(req: Request, res: Response, next: NextFunction) {
+  try {
+    throw new NotImplemented("delete Problem");
+  } catch (error) {
+    next(error);
+  }
+}
 
-function updateProblem(req: Request, res: Response) {}
+function updateProblem(req: Request, res: Response, next: NextFunction) {
+  try {
+    throw new NotImplemented("update Problem");
+  } catch (error) {
+    next(error);
+  }
+}
 
 module.exports = {
   pingProblemController,
