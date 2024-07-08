f0ed7b1d7e4344318158258c911ec5f8c3787d10
Md.Sadiq <mohammadsadiq4950@gmail.com>
2024-07-08 18:02:45 +0530

    Feat: Added SIngleton Pattern for db connection

--- src/config/db.config.ts
+++ src/config/db.config.ts
@@ -1,15 +1,54 @@
 import mongoose from "mongoose";
 const { ATLAS_DB_URL, NODE_ENV } = require("./server.config");
+import logger from "./logger.config";
 
-async function connectToDB() {
-  try {
-    if (NODE_ENV == "development") {
-      await mongoose.connect(ATLAS_DB_URL);
+class DBConnection {
+  private static instance: DBConnection;
+  private uri: string;
+  private isConnected: boolean;
+
+  private constructor(ATLAS_DB_URL: string) {
+    this.uri = ATLAS_DB_URL;
+    this.isConnected = false;
+  }
+
+  public static getInstance(ATLAS_DB_URL: string): DBConnection {
+    if (!DBConnection.instance) {
+      DBConnection.instance = new DBConnection(ATLAS_DB_URL);
     }
-  } catch (error) {
-    console.log("Unable to connect to the DB server");
-    console.log(error);
+    return DBConnection.instance;
+  }
+
+  public async connect(): Promise<void> {
+    if (this.isConnected) {
+      throw new Error('DB Connection already exists');
+    }
+    try {
+      if (NODE_ENV === 'development') {
+        await mongoose.connect(this.uri);
+        this.isConnected = true;
+        logger.info('Database connected successfully');
+      }
+    } catch (error) {
+      logger.error('Error connecting to the database', error);
+      throw error;
+    }
+  }
+
+  public async disconnect(): Promise<void> {
+    if (!this.isConnected) {
+      throw new Error('No DB connection exists to disconnect');
+    }
+    try {
+      await mongoose.disconnect();
+      this.isConnected = false;
+      logger.info('Database disconnected successfully');
+    } catch (error) {
+      logger.error('Error disconnecting from the database', error);
+      throw error;
+    }
   }
 }
 
-module.exports = connectToDB;
+const db = DBConnection.getInstance(ATLAS_DB_URL);
+module.exports = db;
--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ const apiRouter = require("./routes/index");
 const bodyParser = require("body-parser");
 const { PORT } = require("./config/server.config");
 const errorHandler = require("./utils/index");
-const connectDB = require('./config/db.config')
+const db = require('./config/db.config');
 
 const app = express();
 app.use(bodyParser.json());
@@ -20,6 +20,5 @@ app.use(errorHandler.errorHandler); // It is put in last as express has an built
 
 app.listen(PORT, async () => {
   console.log(`server started at ${PORT}`);
-  connectDB();
-  console.log('Connected to Database');
+  await db.connect();
 });
